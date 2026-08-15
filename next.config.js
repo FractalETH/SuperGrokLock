c72a924572da6d92cbd44280ff44fd41b0bc7bb6
Luke <lukejackson07@googlemail.com>
2026-08-15 02:26:02 +0100

    Externalize pino-pretty for Cloudflare build

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ["pino-pretty", "encoding", "pino"],
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       "@x402/evm/upto/client": false,
       "@x402/evm": false,
+      "pino-pretty": false,
+      encoding: false,
     };
-    config.externals = [...(config.externals || []), "pino-pretty", "encoding"];
+    config.externals = [...(config.externals || []), "pino-pretty", "encoding", "pino"];
     return config;
   },
 };
